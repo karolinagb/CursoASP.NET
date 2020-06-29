8d3e608c7733429080c2e49d27ad95d4cba8636e
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-06-29 20:11:23 -0300

    Entidades do projeto

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,9 +1,44 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
+        /*Um departamento pode ter vários vendedores.
+         Vamos implementar um ICollection porque é mais genérico.*/
+        //Uma classe abstrata ou interface não pode ser instanciada, vamos criar uma instância de lista.
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        /*Como teremos um construtor com argumentos, o framework precisa de um construtor vazio/default*/
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //Adicionar ou remove um seller
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        /*Total de vendas do departamento:*/
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            /*Para somar as vendas do departamento, tenho que somar as vendas de todos os vendedores naquele departamento*/
+            /*Para isso vamos pegar cada vendedor da lista, chamar o TotalSales (passando o período) 
+             * do vendedor e somando o resultado*/
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate  { get; set; }
+        public double BaseSalary { get; set; }
+
+        //O seller possui um department:
+        public Department Department { get; set; }
+
+        /*Um seller possui vários SalesRecords*/
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        /*Métodos Customizados*/
+
+        //Adicionar e remover uma venda:
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        //Total de vendas
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //Para descobrir o total de vendas no período vamos utilizar o Linq:
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
